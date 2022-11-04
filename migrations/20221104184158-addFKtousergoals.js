5bf201abaea198c3d4676b85cadc7e463855e318
Eddie Zaboroskie <Zaboroskie.Edward@gmail.com>
2022-11-04 15:20:15 -0400

    added FK columns to tables

---
+++ migrations/20221104184158-addFKtousergoals.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   
+    await queryInterface.addColumn('usergoals', 'usergoalid',{
+    
+    
+      type:Sequelize.INTEGER,
+      references:{
+        model:'Users', key:'id'
+      }
+   
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
