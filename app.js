0416e046079197d951498a62594cfe4c6c505057
Nova Merida <105570301+NovaMerida@users.noreply.github.com>
2022-11-04 16:28:38 -0400

    made a user app.get
    We needed a user app.get so i can work on that

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.get('/login', (req, res)=>{
     res.render('login')
 })
 
+app.get('/user', (req, res) => {
+    res.render('user')
+})
+
 app.get('/register', (req,res)=>{
     res.render('register')
 })
