a42b3f8e1c2553e1084380e9b3f6f9363a0622cd
MattHerringshaw1 <matt.herringshaw@yahoo.com>
2022-11-08 09:46:38 -0600

    register/login POST commit

--- app.js
+++ app.js
@@ -1,114 +1,40 @@
-// APP SETUP
-const express = require('express')
-const app = express()
-const mustacheExpress = require('mustache-express')
-const models = require('./models')
-const bcrypt = require('bcryptjs')
-const cors = require('cors')
-const session = require('express-session')
-const { Op } = require('sequelize') // Operator 
-const formidable = require('formidable')
-const {v4: uuidv4} = require('uuid')
-let uniqueFilename = ''
-require('dotenv').config()
-
-app.use('/uploads',express.static('uploads'))
-app.use('/css', express.static('css'))
-
-app.use(session({
-    secret: 'SuperSecretPassword',
-    saveUninitialized: true
-}))
-
-app.use(express.urlencoded())
-app.engine('mustache', mustacheExpress())
-app.set('views', './views')
-app.set('view engine', 'mustache')
-
-//Function to upload files
-function uploadFile(req,callback){
-    new formidable.IncomingForm().parse(req)
-    .on('fileBegin',(name,file)=>{
-
-        uniqueFilename = `${uuidv4()}.${file.originalFilename.split('.').pop()}`;
-        file.name = uniqueFilename
-        file.filepath = __dirname + '/uploads/' + uniqueFilename
-    })
-    .on('file',(name,file)=>{
-        callback(file.name)
-
-    })
-}
 
 
-
-
-// GET ROUTES
-app.get('/', (req, res) => {
-    res.render('index')
+app.post ("/register", async (req, res) => {
+    const {username, password} = req.body 
+    let salt = await bcrypt.genSalt(10)
+    let hashedPassword = await bcrypt.hash(password, salt)
+    let user = await models.User.findOne({where: {username:username}})
+    if(user) {
+    res.render("register", { errorMessage: 'Username is already taken'})
+    } else {
+    const newUser = models.User.build({
+        username: username,
+        password: hashedPassword
+        })
+    await newUser.save()
+    res.redirect('/login')
+    }
 })
 
-app.get('/login', (req, res)=>{
-    res.render('login')
-})
 
-app.get('/register', (req,res)=>{
-    res.render('register')
-})
-
-app.get('/user', (req,res)=>{
-    
-    res.render('user')
-   
-})
-
-app.get('/phealth', (req,res)=>{
-    res.render('phealth')
-})
-
-app.get('/mhealth',(req,res)=>{
-    res.render('mhealth')
-})
-
-app.get('/add-user-goal', (req,res)=>{
-    res.render('add-user-goal')
-})
-
-app.get('/user-goals', async (req,res)=>{
-    const userGoals = await models.usergoal.findAll({}) 
-    res.render('user-goals', {userGoals: userGoals})
-})
-
-//POST ROUTES
-app.post('/upload',(req,res)=>{
-
-    uploadFile(req,(photoURL)=>{
-        photoURL = `/uploads/${photoURL}`
-        res.render('user', {imageURL: photoURL, className: 'profile-picture' })
-    })
-})
-
-app.post('/add-user-goal', async (req,res)=>{
-    const {goal, description, completed} = req.body
-    
-
-    const userGoal = models.usergoal.build({
-        goal: goal,
-        description: description,
-        completed: completed 
-    })
-
-    const savedGoal = await userGoal.save()
-    if(savedGoal){
-        const userGoals = await models.usergoal.findAll({}) 
-        res.render('user-goals', {userGoals: userGoals})
-    }else{
-        res.send('Not able to create new user goal')
+app.post("/login", async (req, res) => {
+    const {username, password} = req.body 
+    let user = await models.User.findOne({where: {username:username}})
+    if(user) {
+        const result = await bcrypt.compare(password, user.password)
+        if(result) {
+            if(req.session) {
+                req.session.username = user.username 
+            }
+            res.redirect("/user")
+        } else {
+            res.render("login", { errorMessage: "Invalid username"})
+        }
+    } else {
+        res.render("login", { errorMessage: "Enter correct username or password."})
     }
 })
 
 
 
-app.listen(8080,() => {
-    console.log('Server is running healthy!')
-})
